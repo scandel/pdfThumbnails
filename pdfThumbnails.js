ed502e2f1d432708aee730e526610f99c1af1925
bohwaz <github.bohwaz@miam.kd2.org>
2019-10-26 23:59:15 +0200

    * Delay pdf.js loading until we really need it * Update deprecated API calls * Fix path to pdf.js according to README example * Make sure worker is re-used between documents, and not loaded multiple times

--- pdfThumbnails.js
+++ pdfThumbnails.js
@@ -7,47 +7,77 @@
  */
 var createPDFThumbnails = function(){
 
-    if (typeof pdfjsLib === 'undefined') {
-        throw Error("pdf.js is not loaded. Please include it before pdfThumbnails.js.");
-    }
-    pdfjsLib.disableWorker = true;
+    var worker = null;
+    var loaded = false;
+    var renderQueue = [];
 
     // select all img elements with data-pdf-thumbnail-file attribute
     var nodesArray = Array.prototype.slice.call(document.querySelectorAll('img[data-pdf-thumbnail-file]'));
 
-    nodesArray.forEach(function(element) {
-        var filePath = element.getAttribute('data-pdf-thumbnail-file');
-        var imgWidth = element.getAttribute('data-pdf-thumbnail-width');
-        var imgHeight = element.getAttribute('data-pdf-thumbnail-height');
+    if (!nodesArray.length) {
+        // No PDF found, don't load PDF.js
+        return;
+    }
 
-        pdfjsLib.getDocument(filePath).then(function (pdf) {
-            pdf.getPage(1).then(function (page) {
-                var canvas = document.createElement("canvas");
-                var viewport = page.getViewport(1.0);
-                var context = canvas.getContext('2d');
+    if (!loaded && typeof(pdfjsLib) === 'undefined') {
+        var src = document.querySelector('script[data-pdfjs-src]').getAttribute('data-pdfjs-src');
 
-                if (imgWidth) {
-                    viewport = page.getViewport(imgWidth / viewport.width);
-                } else if (imgHeight) {
-                    viewport = page.getViewport(imgHeight / viewport.height);
-                }
+        if (!src) {
+            throw Error('PDF.js URL not set in "data-pdfjs-src" attribute: cannot load PDF.js');
+        }
 
-                canvas.height = viewport.height;
-                canvas.width = viewport.width;
+        var script = document.createElement('script');
+        script.src = src;
+        document.head.appendChild(script).onload = renderThumbnails;
+        loaded = true;
+    }
+    else {
+        renderThumbnails();
+    }
 
-                page.render({
-                    canvasContext: context,
-                    viewport: viewport
-                }).then(function () {
-                    element.src = canvas.toDataURL();
+    function renderThumbnails() {
+        if (!pdfjsLib) {
+            throw Error("pdf.js failed to load. Check data-pdfjs-src attribute.");
+        }
+
+        nodesArray.forEach(function(element) {
+            if (null === worker) {
+                worker = new pdfjsLib.PDFWorker();
+            }
+
+            var filePath = element.getAttribute('data-pdf-thumbnail-file');
+            var imgWidth = element.getAttribute('data-pdf-thumbnail-width');
+            var imgHeight = element.getAttribute('data-pdf-thumbnail-height');
+
+            pdfjsLib.getDocument({url: filePath, worker: worker}).promise.then(function (pdf) {
+                pdf.getPage(1).then(function (page) {
+                    var canvas = document.createElement("canvas");
+                    var viewport = page.getViewport({scale: 1.0});
+                    var context = canvas.getContext('2d');
+
+                    if (imgWidth) {
+                        viewport = page.getViewport({scale: imgWidth / viewport.width});
+                    } else if (imgHeight) {
+                        viewport = page.getViewport({scale: imgHeight / viewport.height});
+                    }
+
+                    canvas.height = viewport.height;
+                    canvas.width = viewport.width;
+
+                    page.render({
+                        canvasContext: context,
+                        viewport: viewport
+                    }).promise.then(function () {
+                        element.src = canvas.toDataURL();
+                    });
+                }).catch(function() {
+                    console.log("pdfThumbnails error: could not open page 1 of document " + filePath + ". Not a pdf ?");
                 });
             }).catch(function() {
-                console.log("pdfThumbnails error: could not open page 1 of document " + filePath + ". Not a pdf ?");
+                console.log("pdfThumbnails error: could not find or open document " + filePath + ". Not a pdf ?");
             });
-        }).catch(function() {
-            console.log("pdfThumbnails error: could not find or open document " + filePath + ". Not a pdf ?");
         });
-    });
+    }
 };
 
 if (
